a23b92673d430ccdf4cf9bfa4470490bb0291c1b
Vladyslav <vladarius23@gmail.com>
2023-01-16 22:29:16 +0100

    created banner

--- index.php
+++ index.php
@@ -4,39 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>Pizza-Hole</title>
+    <link rel="stylesheet" href="./index.css">
+    <title>AniHell</title>
 </head>
 <body>
     <div class="wrapper">
         <div class="banner">
-            <div class="logo">
-                <a href="./index.php"><img src="./images/logo.png" alt="logo"></a>
-            </div>
-            <div class="login">
-                <?php
-                    if (isset($_COOKIE['user'])) {
-                        echo "<button>Sign Out</button>";
-                    }else {
-                        echo "<button>Register</button><button>Sign In</button>";
-                    }
-                ?>
-            </div>
-        </div> 
-        <main>
-            <div class="sidebar">
-                    <a href="">Menu</a>
+                <a class='logoSquare' href="./index.php"><img src="./images/logo.png" alt="AniHell"></a>
+                <div class="menu">
+                    <a href=""><span>Anime</span></a>
+                    <a href=""><span>Manga</span></a>
+                    <a href=""><span>Users</span></a>
                     <?php
-                        if (isset($_COOKIE['user'])) {
-                            echo "<a href=''>Your Orders</a>";
+                        if(isset($_COOKIE['user'])){
+                            echo '<a href=""><span>Sign Out</span></a>';
+                        }else{
+                            echo '<a href=""><span>Profile</span></a>'; 
+                            echo '<a href=""><span>Sign In</span></a>';
                         }
                     ?>
-                    <a href="">Contacts</a>
-            </div>
-            <div class="content">
-                Pizza!
-            </div>
-        </main> 
+                </div>
+        </div>
     </div>
 </body>
 </html>
